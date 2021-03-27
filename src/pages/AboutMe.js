a45b30c18afea8c561882ad1dd84f1939f11fea8
rxtatx <rachel.thiim@gmail.com>
2021-03-26 19:54:47 -0500

    Satified basic requirements for issue #6

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,13 @@
+import dog from '../dog.jpg';
+
 const AboutMe = () => {
     return (
-        <h1>
-            About Me
-        </h1>
+        <>
+            <h1>About Me</h1>
+            <img src={dog} alt="Me [as a dog] at work" />
+            <blockquote>Doggo ipsum length boy very taste wow porgo length boy ruff, sub woofer blep mlem. Blep boofers clouds heckin good boys and girls heckin good boys, noodle horse wrinkler. You are doing me a frighten heckin good boys and girls the neighborhood pupper shoober shoob very jealous pupper heckin puggorino, fluffer sub woofer fat boi very hand that feed shibe porgo. Such treat long woofer floofs heckin good boys and girls boof, doing me a frighten super chub. Puggo big ol long bois very taste wow wrinkler, ur givin me a spook wrinkler. Borking doggo fluffer smol borking doggo with a long snoot for pats wrinkler pupperino borkf, puggo you are doin me a concern fluffer thicc. Pats what a nice floof shibe very hand that feed shibe, maximum borkdrive big ol mlem, woofer clouds. Puggorino heckin borkdrive, borkf.
+            </blockquote>
+        </>
     )
 }
 
